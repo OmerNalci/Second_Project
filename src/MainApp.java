c9b76d09c896eb7332089e01e0d0e065f55ca463
Omer Nalci <oemer.nalci@gmail.com>
2022-10-16 15:16:31 +0200

    Dünya degisti.

---
+++ src/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * Baslangic sinifidir.
+ * @link
+ * @see
+ */
+public class MainApp {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba..");
+        System.out.println("Dünya");
+
+
+
+
+    }
+}
